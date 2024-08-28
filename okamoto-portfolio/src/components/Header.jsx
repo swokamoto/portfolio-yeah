4c397f6d5d774cb462a5728db9acbaf47ed84b4b
swokamoto <swokamoto@gmail.com>
2024-08-27 22:51:39 -0400

    added pages

---
+++ okamoto-portfolio/src/components/Header.jsx
@@ -0,0 +1,18 @@
+export default function Header() {
+    return (
+        <header>
+
+            <h1>Okamoto Portfolio</h1>
+            
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Projects</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+            
+        </header>
+    )
+}
---
+++ okamoto-portfolio/src/components/Project.jsx
@@ -0,0 +1,19 @@
+export default function Project() {
+    return (
+        <section id="projects">
+            <h2>Projects</h2>
+            <div className="project">
+                <h3>Project 1</h3>
+                <p>Project description</p>
+            </div>
+            <div className="project">
+                <h3>Project 2</h3>
+                <p>Project description</p>
+            </div>
+            <div className="project">
+                <h3>Project 3</h3>
+                <p>Project description</p>
+            </div>
+        </section>
+    )
+}
